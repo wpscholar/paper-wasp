c4adcad4a6ddc42f25d29d1e9a6cdd54ac127478
Micah Wood <micah@wpscholar.com>
2017-06-08 11:26:51 -0400

    Updated selectors to take into account not just permissions on the component type registration, but also the component data.

--- source/js/modules/paper-wasp-editor/selectors.js
+++ source/js/modules/paper-wasp-editor/selectors.js
@@ -65,15 +65,136 @@ export const getCanAdd = createSelector(
 );
 
 /**
+ * Checks if a specific component can have components appended to it.
+ *
+ * @param state {State}
+ * @param canAppend {boolean}
+ */
+export const getCanAppendComponent = (state: State, {canAppend = true}: Component) => canAppend;
+
+/**
+ * Checks if a user can append to a specific type of component.
+ *
+ * @param componentTypeObject {Object}
+ * @returns {boolean}
+ */
+export const getCanAppendComponentType = createSelector(
+    [getComponentTypeObject],
+    ({canAppend = true}) => canAppend
+);
+
+/**
+ * Checks if a user can append to a specific component.
+ *
+ * @param canAppendType {boolean}
+ * @param canAppendComponent {boolean}
+ * @returns {boolean}
+ */
+export const getCanAppend = createSelector(
+    [getCanAppendComponentType, getCanAppendComponent],
+    (canAppendType, canAppendComponent) => canAppendType && canAppendComponent
+);
+
+/**
+ * Checks if a specific component can be deleted.
+ *
+ * @param state {State}
+ * @param canDelete {boolean}
+ */
+export const getCanDeleteComponent = (state: State, {canDelete = true}: Component) => canDelete;
+
+/**
  * Checks if a user can delete a specific type of component.
  *
  * @param componentTypeObject {Object}
  * @returns {boolean}
  */
-export const getCanDelete = createSelector(
+export const getCanDeleteComponentType = createSelector(
     [getComponentTypeObject],
-    ({canDelete}) => canDelete
+    ({canDelete = true}) => canDelete
 );
+
+/**
+ * Checks if a user can delete a specific component.
+ *
+ * @param canDeleteType {boolean}
+ * @param canDeleteComponent {boolean}
+ * @returns {boolean}
+ */
+export const getCanDelete = createSelector(
+    [getCanDeleteComponentType, getCanDeleteComponent],
+    (canDeleteType, canDeleteComponent) => canDeleteType && canDeleteComponent
+);
+
+/**
+ * Checks if a specific component can be dragged.
+ *
+ * @param state {State}
+ * @param canDrag {boolean}
+ */
+export const getCanDragComponent = (state: State, {canDrag = true}: Component) => canDrag;
+
+/**
+ * Checks if a user can drag a specific type of component.
+ *
+ * @param componentTypeObject {Object}
+ * @returns {boolean}
+ */
+export const getCanDragComponentType = createSelector(
+    [getComponentTypeObject],
+    ({canDrag = true}) => canDrag
+);
+
+/**
+ * Checks if a user can drag a specific component.
+ *
+ * @param canDragType {boolean}
+ * @param canDragComponent {boolean}
+ * @returns {boolean}
+ */
+export const getCanDrag = createSelector(
+    [getCanDragComponentType, getCanDragComponent],
+    (canDragType, canDragComponent) => canDragType && canDragComponent
+);
+
+/**
+ * Checks if a specific component can be dropped.
+ *
+ * @param state {State}
+ * @param canDrop {boolean}
+ */
+export const getCanDropComponent = (state: State, {canDrop = true}: Component) => canDrop;
+
+/**
+ * Checks if a user can drop a specific type of component.
+ *
+ * @param componentTypeObject {Object}
+ * @returns {boolean}
+ */
+export const getCanDropComponentType = createSelector(
+    [getComponentTypeObject],
+    ({canDrop = true}) => canDrop
+);
+
+/**
+ * Checks if a user can drop a specific component.
+ *
+ * @param canDropType {boolean}
+ * @param canDropComponent {boolean}
+ * @returns {boolean}
+ */
+export const getCanDrop = createSelector(
+    [getCanDropComponentType, getCanDropComponent],
+    (canDropType, canDropComponent) => canDropType && canDropComponent
+);
+
+/**
+ * Checks if a specific component can be edited.
+ *
+ * @param state {State}
+ * @param canEdit {Boolean}
+ */
+export const getCanEditComponent = (state: State, {canEdit = true}: Component) => canEdit;
 
 /**
  * Checks if a user can edit a specific type of component.
@@ -81,9 +202,21 @@ export const getCanDelete = createSelector(
  * @param componentTypeObject {Object}
  * @returns {boolean}
  */
-export const getCanEdit = createSelector(
+export const getCanEditComponentType = createSelector(
     [getComponentTypeObject],
-    ({canEdit}) => canEdit
+    ({canEdit = true}) => canEdit
+);
+
+/**
+ * Checks if a user can edit a specific component.
+ *
+ * @param canEditType {boolean}
+ * @param canEditComponent {boolean}
+ * @returns {boolean}
+ */
+export const getCanEdit = createSelector(
+    [getCanEditComponentType, getCanEditComponent],
+    (canEditType, canEditComponent) => canEditType && canEditComponent
 );
 
 /**
