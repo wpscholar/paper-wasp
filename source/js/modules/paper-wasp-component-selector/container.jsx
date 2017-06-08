7ee3198b7e7aff41e2db76fb60475da5af536832
Micah Wood <micah@wpscholar.com>
2017-06-08 15:41:25 -0400

    Ensure that the added component’s editor shows after an add occurs. If the component doesn’t support an editor, just close the modal. This means we can just delete the editorClass if we don’t need it.

--- source/js/modules/paper-wasp-component-selector/container.jsx
+++ source/js/modules/paper-wasp-component-selector/container.jsx
@@ -2,9 +2,9 @@
 import filter from 'lodash.filter';
 import {connect} from 'react-redux';
 import {addComponent} from 'paper-wasp/action-creators';
-import {getLastAddedComponentId} from 'paper-wasp/selectors';
 import {getActiveComponentId} from 'paper-wasp-editor/selectors';
 import {editComponent} from 'paper-wasp-editor/functions';
+import {closeModal} from 'paper-wasp-editor/action-creators';
 
 import ComponentSelector from './component';
 
@@ -19,8 +19,20 @@ const PaperWaspComponentSelector = connect(
     }),
     dispatch => ({
         addComponent: (parent, type, store) => {
-            dispatch(addComponent({parent, type}));
-            editComponent(dispatch, getLastAddedComponentId(store.getState()));
+            // Explicity set the uid, so we know what it is
+            const uid = Date.now();
+            // Add the component
+            dispatch(addComponent({parent, type, uid}));
+            // Get the component registry
+            const {componentRegistry} = store.getState().container;
+            // Determine if the component we are adding has an editor
+            if (componentRegistry.hasProperty(type, 'editorClass')) {
+                // If so, load the editor
+                editComponent(dispatch, uid);
+            } else {
+                // If not, just close the modal
+                dispatch(closeModal());
+            }
         }
     })
 )(ComponentSelector);
