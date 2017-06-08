9e6ff4c9d18e11baba0f8dad7aa94b730f5f65d5
Micah Wood <micah@wpscholar.com>
2017-06-08 11:24:54 -0400

    Added middleware for setting default children

---
+++ source/js/modules/paper-wasp-default-children-middleware/index.js
@@ -0,0 +1,41 @@
+import {ADD_COMPONENT, insertComponents} from 'paper-wasp/action-creators';
+import {getLastAddedComponentId} from 'paper-wasp/selectors';
+
+export default function (componentRegistry) {
+
+    return store => next => (action) => {
+
+        const dispatched = next(action);
+
+        const lastAddedId = getLastAddedComponentId(store.getState());
+
+        if (action.type === ADD_COMPONENT) {
+
+            const defaultChildren = componentRegistry.getProperty(action.componentType, 'defaultChildren', action.defaultChildren || []);
+
+            if (defaultChildren && defaultChildren.length) {
+
+                const children = [];
+
+                defaultChildren.forEach((defaultChild) => {
+
+                    const child = Object.assign({parent: 0}, defaultChild);
+
+                    if (child.parent === 0) {
+                        child.parent = lastAddedId;
+                    }
+
+                    children.push(child);
+
+                });
+
+                store.dispatch(insertComponents(children));
+            }
+
+        }
+
+        return dispatched;
+
+    };
+
+}
