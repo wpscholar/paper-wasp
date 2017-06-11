54ca5ff91e535adda8cfecdb7b551a395e66db94
Micah Wood <micah@wpscholar.com>
2017-06-11 00:51:18 -0400

    Updated rich text to use new decorator approach.

--- source/js/components/paper-wasp-component-rich-text/index.js
+++ source/js/components/paper-wasp-component-rich-text/index.js
@@ -1,2 +1,19 @@
-export * from './component';
-export * from './component-editor';
+// @flow
+
+import {RichText} from './component';
+import {RichTextEditor} from './component-editor';
+
+const registerRichText = {
+    canAdd: true,
+    canDelete: true,
+    canEdit: true,
+    class: RichText,
+    classEditor: RichTextEditor,
+    group: 'component',
+    isDynamic: false,
+    label: 'Rich Text',
+    thumbnailUrl: '/assets/img/rich-text.png',
+    type: 'rich-text',
+};
+
+export {RichText, RichTextEditor, registerRichText};
