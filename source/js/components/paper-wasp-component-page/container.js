227ab32b121379c6cf892f5622f21b346aac995c
Micah Wood <micah@wpscholar.com>
2017-06-11 00:50:59 -0400

    Updated page to use containers

---
+++ source/js/components/paper-wasp-component-page/container.js
@@ -0,0 +1,8 @@
+// @flow
+import {ComponentDecorator} from 'paper-wasp-component/decorator';
+
+import Component from './component';
+
+const Page = ComponentDecorator(Component);
+
+export {Page};
--- source/js/components/paper-wasp-component-page/index.js
+++ source/js/components/paper-wasp-component-page/index.js
@@ -1,19 +1,2 @@
-// @flow
-import {connect} from 'react-redux';
-import {onDragOver, onDrop} from 'paper-wasp-editor/functions';
-import {ComponentDecorator} from 'paper-wasp-component/decorator';
-
-import PaperWaspPage from './component';
-import EditMode from './component-edit-mode';
-
-const Page = ComponentDecorator(PaperWaspPage);
-
-const PageEditMode = connect(
-    null,
-    dispatch => ({
-        onDragOver,
-        onDrop: e => onDrop(e, dispatch)
-    })
-)(ComponentDecorator(EditMode));
-
-export {Page, PageEditMode};
+export * from './container';
+export * from './container-edit-mode';
