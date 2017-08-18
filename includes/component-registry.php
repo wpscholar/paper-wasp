a548c3a34cbaeac945912f85f8923d84972a77a0
Micah Wood <micah@wpscholar.com>
2017-08-18 17:03:40 -0400

    Always load the minified component JS files

--- includes/component-registry.php
+++ includes/component-registry.php
@@ -1,36 +1,36 @@
 <?php
 
 paper_wasp_register_component( 'paper-wasp-button', 'Button', [
-    'class' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-button/assets/component.js',
-    'classEditor' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-button/assets/component-editor.js',
+    'class' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-button/assets/component.min.js',
+    'classEditor' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-button/assets/component-editor.min.js',
     'thumbnailUrl' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-button/assets/button.png',
 ] );
 
 
 paper_wasp_register_component( 'paper-wasp-heading', 'Heading', [
-    'class' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-heading/assets/component.js',
-    'classEditor' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-heading/assets/component-editor.js',
+    'class' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-heading/assets/component.min.js',
+    'classEditor' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-heading/assets/component-editor.min.js',
     'thumbnailUrl' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-heading/assets/heading.png',
 ] );
 
 paper_wasp_register_component( 'paper-wasp-image', 'Image', [
-    'class' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-image/assets/component.js',
-    'classEditor' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-image/assets/component-editor.js',
+    'class' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-image/assets/component.min.js',
+    'classEditor' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-image/assets/component-editor.min.js',
     'thumbnailUrl' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-heading/assets/image.jpeg',
 ] );
 
 paper_wasp_register_component( 'paper-wasp-pull-quote', 'Pull Quote', [
-    'class' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-pull-quote/assets/component.js',
-    'classEditor' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-pull-quote/assets/component-editor.js',
+    'class' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-pull-quote/assets/component.min.js',
+    'classEditor' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-pull-quote/assets/component-editor.min.js',
 ] );
 
 paper_wasp_register_component( 'paper-wasp-quote', 'Quote', [
-    'class' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-quote/assets/component.js',
-    'classEditor' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-quote/assets/component-editor.js',
+    'class' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-quote/assets/component.min.js',
+    'classEditor' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-quote/assets/component-editor.min.js',
 ] );
 
 paper_wasp_register_component( 'paper-wasp-video', 'Video', [
-    'class' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-video/assets/component.js',
-    'classEditor' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-video/assets/component-editor.js',
+    'class' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-video/assets/component.min.js',
+    'classEditor' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-video/assets/component-editor.min.js',
     'thumbnailUrl' => PAPER_WASP_URL . 'source/js/components/paper-wasp-component-video/assets/video.png',
 ] );
